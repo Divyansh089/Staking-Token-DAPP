b872bfac0d42d679b70198edac3c2d556f9297b6
Divyansh Patel <pateldivyansh131@gmail.com>
2026-05-15 10:30:00 +0530

    feat: implement ButtonCmp with hero__btn gradient styling

--- Components/Admin/RegularComp/ButtonCmp.jsx
+++ Components/Admin/RegularComp/ButtonCmp.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const ButtonCmp = () => {
-  return <div>ButtonCmp</div>;
+const ButtonCmp = ({ title, handleClick, icon, style }) => {
+  return (
+    <button
+      onClick={handleClick}
+      className="hero__btn"
+      style={{
+        width: "auto",
+        minWidth: "180px",
+        marginTop: "20px",
+        cursor: "pointer",
+        ...style,
+      }}
+    >
+      {icon && <span style={{ marginRight: "8px" }}>{icon}</span>}
+      {title}
+    </button>
+  );
 };
 
 export default ButtonCmp;
