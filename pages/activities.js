a5861ae6037a40f225ba17a6b0ab4e245950b309
Divyansh Patel <pateldivyansh131@gmail.com>
2026-05-15 13:50:00 +0530

    feat: implement Activities page for transaction history

--- pages/activities.js
+++ pages/activities.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import { Header, Footer, Notification, Loader } from "../Components/index";
+import { CONTRACT_DATA } from "../Context/index";
 
-const activities = () => {
-  return <div>activities</div>;
+const Activities = () => {
+  const { address } = useAccount();
+  const [loader, setLoader] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (address) {
+        setLoader(true);
+        const data = await CONTRACT_DATA(address);
+        setNotifications(data?.notifications || []);
+        setLoader(false);
+      }
+    };
+    fetchData();
+  }, [address]);
+
+  return (
+    <div>
+      <Header />
+      {loader && <Loader />}
+      <div style={{ paddingTop: "80px" }}>
+        <Notification notifications={notifications} />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
-export default activities;
+export default Activities;
